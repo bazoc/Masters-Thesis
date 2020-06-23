88e522890e2c42a7a832ed27c83bb63d8a265bc9
bazoc <barry.o-kane@ucdconnect.ie>
2020-06-23 18:30:58 +0100

    Added alot of the methods for BVAR

--- VAR with Assets, Volatility.R
+++ VAR with Assets, Volatility.R
@@ -11,6 +11,8 @@ library(tidyverse)
 library(BVAR)
 library(plm)
 library(panelvar)
+library(coda)
+library(parallel)
 #Year we are using
 sta <- 2008
 fin <- 2019 
@@ -190,3 +192,29 @@ bvar()
 for(i in 1:length(countries)) {
   model2[[countries[i]]] <- bvar(data = data[[countries[i]]], lags = k, bv_irf = c(identification = FALSE))
 }
+names(model2$Austria$beta)
+model2$Austria$variables
+
+summary(model2$Austria)
+apply(model2$Austria$beta, FUN = mean, MARGIN = 2)
+plot(model2$Austria)
+plot(model2$Austria, type = "dens", vars_response = "lgdp", vars_impulse = "lgdp-lag1")
+
+#Make it a coda object
+austria_mcmc <- as.mcmc(model2$Austria)
+
+#Multiple chains
+n_cores <- 2
+cl <- makeCluster(n_cores)
+
+#Run the model
+for(i in 1:length(countries)) {
+  model2[[countries[i]]] <- par_bvar(cl = cl, data = data[[countries[i]]], lags = k, bv_irf = c(identification = FALSE))
+}
+yup <- par_bvar(cl = cl, data = data$Austria, lags = k, n_draw = 60000, n_burn = 10000, n_thin = 20)
+stopCluster(cl)
+#Make it a coda object
+austria_mcmc <- as.mcmc(yup)
+gelman.plot(austria_mcmc)
+effectiveSize(austria_mcmc)
+acf(austria_mcmc$x)
